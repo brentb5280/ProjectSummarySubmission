49229a662cedbe8e2eefec1d036ca81692e66a49
Brent Bowles <bbowles@sileotech.com>
2021-08-20 19:40:05 -0700

    PHP

--- contact.php
+++ contact.php
@@ -1,9 +1,13 @@
 <?php
-$to = 'brentbowles111@yahoo.com' //sendto@example.com
+$to = 'brentbowles111@yahoo.com' ; //sendto@example.com
 $subject = 'The subject';
-$body = 'The email body content';
-$headers = array('Content-Type: text/html; charset=UTF-8');
+$body = "Document Title: " . $_POST["title"];
+$body .= "Due Date: " . $_POST["due_date"];
+$body .= "industry: " . $_POST["industry1"];
+$body .= "industry: " . $_POST["industry2"];
+//$headers = array('Content-Type: text/html; charset=UTF-8');
 
-
-wp_mail( $to, $subject, $body, $headers );
+echo "Your Form has been Submitted";
+$result = mail( $to, $subject, $body);
+print_r($result);
 ?>
