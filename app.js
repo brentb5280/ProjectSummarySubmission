50aa3afdec0f9f8484aed8ba060dfc0431648303
Brent Bowles <bbowles@sileotech.com>
2021-09-29 20:32:12 -0700

    Nodemailer

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.use('/public', express.static(path.join(dirname, 'public')));
+
 //body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,9 +20,49 @@ app.use(bodyParser.json());
 //app.use('/public', express.static(path.join(_dirname, 'public')));
 
 app.get('/', (req,res) =>{
-    res.send('Hello');
+    res.render('contact');
 });
 
+app.post('/send', (req, res) =>{
+  const output = `
+  <p> you have a new submission </p>
+  <h3> Submission form</h3>
+    <ul>
+    <li>Name: ${re.body.name}</li>
+    <li>Company: ${re.body.company}</li>
+    <li>Email: ${re.body.email}</li>
+    <li>Phone: ${re.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.sendgrid.net",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: 'brentbowles111@yahoo.com', // generated ethereal user
+      pass: 'zaq12wsxXSW@!QAZ', // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"nodemailer contact" <brentbowles111@yahoo.com>', // sender address
+    to: "bbowles@sileotech.com, tech@sileotech.com", // list of receivers
+    subject: "Node request", // Subject line
+    text: "Hello world?", // plain text body
+    html: output, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+   res.render('contact', {msg:'email has been sent'})
+
+});
 app.listen(3001, () => console.log('server started...'));
 
 //let transporter = nodemailer.createTransport({
